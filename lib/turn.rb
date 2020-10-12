7baff11576ba7bdd86c163425e54bcbf8dfff83e
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-12 16:20:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,3 +15,7 @@ end
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+      
